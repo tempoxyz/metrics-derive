714adcf7df01d10e23fd8e6d4403cdd935a9d23c
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2026-01-31 00:57:32 +0100

    perf: call with_recorder only once per method (#5)

--- tests/metrics.rs
+++ tests/metrics.rs
@@ -1,16 +1,17 @@
+// Use `cargo nextest` to run tests.
+
 #![allow(missing_docs)]
 use metrics::{
     Counter, Gauge, Histogram, Key, KeyName, Label, Metadata, Recorder, SharedString, Unit,
 };
 use metrics_derive::Metrics;
-use serial_test::serial;
 use std::{
     collections::HashMap,
     sync::{LazyLock, Mutex},
 };
 
 #[allow(dead_code)]
-#[derive(Metrics)]
+#[derive(Clone, Metrics)]
 #[metrics(scope = "metrics_custom")]
 struct CustomMetrics {
     #[metric(skip)]
@@ -114,7 +115,6 @@ fn test_describe(scope: &str) {
 }
 
 #[test]
-#[serial]
 fn describe_metrics() {
     let _guard = RECORDER.enter();
 
@@ -124,7 +124,6 @@ fn describe_metrics() {
 }
 
 #[test]
-#[serial]
 fn describe_dynamic_metrics() {
     let _guard = RECORDER.enter();
 
@@ -168,7 +167,6 @@ fn test_register(scope: &str) {
 }
 
 #[test]
-#[serial]
 fn register_metrics() {
     let _guard = RECORDER.enter();
 
@@ -178,7 +176,6 @@ fn register_metrics() {
 }
 
 #[test]
-#[serial]
 fn register_dynamic_metrics() {
     let _guard = RECORDER.enter();
 
@@ -218,7 +215,6 @@ fn test_labels(scope: &str) {
 }
 
 #[test]
-#[serial]
 fn label_metrics() {
     let _guard = RECORDER.enter();
 
@@ -228,7 +224,6 @@ fn label_metrics() {
 }
 
 #[test]
-#[serial]
 fn dynamic_label_metrics() {
     let _guard = RECORDER.enter();
 
